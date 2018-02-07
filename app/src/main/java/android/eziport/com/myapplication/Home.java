7c7a318ce9c8c319d09985c3d4f1fcad55aef40d
Mahir Gulzar <mahirgulzar@gmail.com>
2018-02-07 16:52:20 +0200

    Search Transferer Layout Added

--- app/src/main/java/android/eziport/com/myapplication/Home.java
+++ app/src/main/java/android/eziport/com/myapplication/Home.java
@@ -29,7 +29,7 @@ public class Home extends AppCompatActivity {
         sender.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Home.this, SenderFormActivity.class);
+                Intent intent = new Intent(Home.this, SearchTransferer.class);
                 startActivity(intent);
             }
         });
---
+++ app/src/main/java/android/eziport/com/myapplication/Sender_Request_Class.java
@@ -0,0 +1,14 @@
+package android.eziport.com.myapplication;
+
+/**
+ * Created by Mahir Gulzar on 2/7/2018.
+ */
+
+public class Sender_Request_Class {
+    String title;
+    String destination;
+    int weight;
+    String fromDate;
+    String toDate;
+    String details;
+}
