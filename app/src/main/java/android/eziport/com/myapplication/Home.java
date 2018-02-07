38131d0220f17234ab83090ad6032fb51a852f2f
Mahir Gulzar <mahirgulzar@gmail.com>
2018-02-07 18:04:56 +0200

    Activity Tweaks and Search List Fragment

--- app/src/main/java/android/eziport/com/myapplication/Home.java
+++ app/src/main/java/android/eziport/com/myapplication/Home.java
@@ -4,6 +4,8 @@ import android.app.Notification;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,17 +16,17 @@ public class Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        Button status = (Button) findViewById(R.id.button1);
+//        Button status = (Button) findViewById(R.id.button1);
         Button sender = (Button) findViewById(R.id.button2);
         Button transfer = (Button) findViewById(R.id.button3);
 
-        status.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Home.this, NotificationActivity.class);
-                startActivity(intent);
-            }
-        });
+//        status.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(Home.this, NotificationActivity.class);
+//                startActivity(intent);
+//            }
+//        });
 
         sender.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,5 +43,30 @@ public class Home extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.notification_menu) {
+            Intent intent = new Intent(Home.this, NotificationActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/android/eziport/com/myapplication/SearchTransfererFragment.java
+++ app/src/main/java/android/eziport/com/myapplication/SearchTransfererFragment.java
@@ -2,21 +2,38 @@ package android.eziport.com.myapplication;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class SearchTransfererFragment extends Fragment {
+public class SearchTransfererFragment extends ListFragment implements AdapterView.OnItemClickListener{
 
-    public SearchTransfererFragment() {
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.content_search_transferer, container, true);
+        return view;
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_search_transferer, container, false);
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.Planets, android.R.layout.simple_list_item_1);
+        setListAdapter(adapter);
+        getListView().setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position,long id) {
+        Toast.makeText(getActivity(), "Item: " + position, Toast.LENGTH_SHORT).show();
     }
 }
