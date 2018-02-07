60483cd2ab48705ea8f4533d7fb8060d5d0d6953
andreasvija <andreasvija@hotmail.com>
2018-02-07 14:01:31 +0200

    added sender form data variables

--- app/src/main/java/android/eziport/com/myapplication/SenderFormActivity.java
+++ app/src/main/java/android/eziport/com/myapplication/SenderFormActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SenderFormActivity extends AppCompatActivity {
 
@@ -21,6 +23,21 @@ public class SenderFormActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                EditText destinationBox = (EditText) findViewById(R.id.destinationBox);
+                String destination = destinationBox.getText().toString();
+
+                EditText weightBox = (EditText) findViewById(R.id.weightBox);
+                int weight = Integer.valueOf(weightBox.getText().toString());
+
+                EditText fromDateBox = (EditText) findViewById(R.id.fromDateBox);
+                String fromDate = fromDateBox.getText().toString();
+
+                EditText toDateBox = (EditText) findViewById(R.id.toDateBox);
+                String toDate = toDateBox.getText().toString();
+
+                EditText detailsBox = (EditText) findViewById(R.id.detailsBox);
+                String details = detailsBox.getText().toString();
+
                 Intent intent = new Intent(getApplicationContext(), Home.class);
                 startActivity(intent);
                 finish();
